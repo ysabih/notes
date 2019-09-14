768113f53ff00b8da0200209dc2f59fad672d03d
Yussuf <sabihyussuf@gmail.com>
2019-09-14 14:20:56 +0100

    use material-ui

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
-import Grid from '@material-ui/core/Grid';
-import './App.css';
-
+import React from 'react';
+import GlobalAppBar from './GlobalAppBar';
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <Grid container direction="column" justify="center" alignItems="center">
-            <h1>You clicked {count} times</h1>
-            <button onClick={() => setCount(count + 1)}>
-                Increment
-            </button>
-        </Grid>);
+    return(
+    <>
+        <GlobalAppBar></GlobalAppBar>
+    </>);
 }
 
 export default App;
---
+++ Client/src/components/Note.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from '@material-ui/core';
+
+const Note = (props) => {
+    return (
+        <Card >
+            
+        </Card>
+    );
+};
+
+export default Note;
