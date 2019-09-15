f2670502a889cb16b39965e7e5aeb3f607b99231
Yussuf <sabihyussuf@gmail.com>
2019-09-15 13:53:10 +0100

    Revert last 3 commits

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import GlobalAppBar from './GlobalAppBar';
+import './App.css';
+
 
 function App() {
-    return(
-    <>
-        <GlobalAppBar></GlobalAppBar>
-    </>);
+  return (
+    <h1>Hello World</h1>
+  );
 }
 
 export default App;
