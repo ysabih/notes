50f7263800f14de3a41bf204c1ce0cc1b84bad8b
Yussuf <sabihyussuf@gmail.com>
2019-10-20 19:12:15 +0100

    Implement modification of existing notes

--- Client/src/components/NoteEditor.js
+++ Client/src/components/NoteEditor.js
@@ -9,17 +9,20 @@ import LoadingSpinner from './LoadingSpinner'
 const NoteEditor = (props) => {
     const [contentFetchFailed, setContentFetchFailed] = useState(false);
     const [noteNotFound, setNoteNotFound] = useState(false);
-    const [loadingNote, setLoadingNote] = useState(false);
-    const [note, setNote] = useState({id: null, title: "", content: ""});
+    const [runningBlockingOperation, setRunningBlockingOperation] = useState(false);
+
+    const [noteId, setNoteId] = useState(null);
+    const [noteTitle, setNoteTitle] = useState('');
+    const [noteContent, setNoteContent] = useState('');
 
     let routeNoteId = null;
     if(props.match !== undefined){
         routeNoteId = props.match.params.noteId;
     }
-    
+
     useEffect(() => {
         if(routeNoteId != null){
-            setLoadingNote(true);
+            setRunningBlockingOperation(true);
             fetch(`/api/notes/${routeNoteId}`)
             .then((response) => {
                 if(response.status === 404){
@@ -27,15 +30,16 @@ const NoteEditor = (props) => {
                     return;
                 }
                 response.json().then((json)=> {
-                    setNote(json);
+                    console.log("Note content recevied: "+JSON.stringify(json, null, 2));
+                    setNoteInState(json);
                 })
             }, (failureReason) => {
                 setContentFetchFailed(true);
-            }).finally(() => setLoadingNote(false));
+            }).finally(() => setRunningBlockingOperation(false));
         }
     }, []);
 
-    function renderNoteContentForm(note){
+    function renderNoteContentForm(){
         return (
         <div className="container-fluid mt-1">
             <div className="container-fluid mb-2 pl-0">
@@ -43,27 +47,30 @@ const NoteEditor = (props) => {
                     <Link className="btn btn-link btn-lg" to="/">
                         <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
                     </Link>
-                    <button className="btn btn-primary btn-xs ml-auto px-3">
+                    <button className="btn btn-primary btn-xs ml-auto px-3" onClick={() => saveNote()}>
                         Save
                     </button>
                 </div>
             </div>
             <div>
-                {loadingNote ? <LoadingSpinner /> : <></>}
+                {runningBlockingOperation ? <LoadingSpinner /> : <></>}
                 <input 
                     className="form-control form-control-lg form-group font-weight-bold" 
                     type="text" 
-                    placeholder= {loadingNote ? "" : "Title"}
-                    disabled = {loadingNote}
-                    autoFocus value={note.title} 
-                    onChange={(event) => setNote({ title: event.target.value })} 
+                    placeholder= {runningBlockingOperation ? "" : "Title"}
+                    disabled = {runningBlockingOperation}
+                    autoFocus value={noteTitle} 
+                    onChange={(event) => {
+                        console.log("Title changed to: "+event.target.value);
+                        setNoteTitle(event.target.value);
+                    }} 
                 />
                 <textarea
                     className="form-control form-group" rows="25" 
-                    placeholder= {loadingNote ? "" : "Note something..." }
-                    disabled = {loadingNote}
-                    value={note.content} 
-                    onChange={(event) => setNote({ content: event.target.value })}
+                    placeholder= {runningBlockingOperation ? "" : "Note something..." }
+                    disabled = {runningBlockingOperation}
+                    value={noteContent} 
+                    onChange={(event) => setNoteContent(event.target.value)}
                 />
             </div>
         </div>);
@@ -83,13 +90,43 @@ const NoteEditor = (props) => {
 
     function renderContent(){
         if(!contentFetchFailed && !noteNotFound){
-            return renderNoteContentForm(note);
+            return renderNoteContentForm();
         }
         if(noteNotFound){
             return renderNotFound();
         }
         if(contentFetchFailed){
             return renderContentFetchFailed();
+        }
+    }
+
+    async function saveNote() {
+        try{
+            setRunningBlockingOperation(true);
+            let url = `/api/notes/${noteId}`;
+            let body = JSON.stringify(getNoteInState());
+            let response = await fetch(url, {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: body});
+            if(response.status === 200){
+                let json = await response.json();
+                console.log("lastModified: " + json.lastModified);
+            }
+        }
+        finally{
+            setRunningBlockingOperation(false);
+        }
+    }
+    
+    function setNoteInState(note) {
+        setNoteId(note.id);
+        setNoteTitle(note.title);
+        setNoteContent(note.content);
+    }
+
+    function getNoteInState() {
+        return {
+            id: noteId,
+            title: noteTitle,
+            content: noteContent
         }
     }
 
