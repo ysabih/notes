eae1388b438c475e411df61a81234da3c21cb479
Yussuf <sabihyussuf@gmail.com>
2020-08-10 17:53:08 +0100

    go to private home page if user is logged in

--- Client/src/components/Public.js
+++ Client/src/components/Public.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {useState, useContext, useEffect}from 'react';
+import { AuthContext } from "../providers/authProvider";
+import Home from './Home';
 
 export const Public = () => {
-    return (
-        <h2>
-        Home
-        </h2>
-    );
+
+    const [user, setUser] = useState(null);
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.getUserAsync().then((user) => {
+            setUser(user);
+        })
+    }, []);
+
+    function renderContent() {
+        if(user){
+            return <Home/>
+        }
+        else{
+            return <h2>Welcome, Login or Sign up</h2>
+        }
+    }
+    
+    return renderContent();
 };
